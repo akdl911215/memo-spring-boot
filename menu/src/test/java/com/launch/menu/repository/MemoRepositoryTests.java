153e3501f072eda4f48c002f2af6945bc11432f5
akdl911215 <akdl911215@naver.com>
2024-01-12 23:18:23 +0900

    spring test

--- menu/src/test/java/com/launch/menu/repository/MemoRepositoryTests.java
+++ menu/src/test/java/com/launch/menu/repository/MemoRepositoryTests.java
@@ -1,10 +1,12 @@
 package com.launch.menu.repository;
 
 import com.launch.menu.entity.Memo;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -13,6 +15,40 @@ public class MemoRepositoryTests {
     @Autowired
     MemoRepository memoRepository;
 
+    @Test
+    public void testUpdate() {
+        Memo memo = Memo.builder().
+    }
+
+    @Transactional
+    @Test
+    public void testSelect2() {
+
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Memo memo = memoRepository.getOne(mno);
+
+        System.out.println("===============================");
+
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testSelect() {
+
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("===============================");
+
+        if (result.isPresent()){
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
 
     @Test
     public void testInsertDummies() {
